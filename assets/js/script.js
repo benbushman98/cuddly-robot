2310e3d1cf47c351362410ae5071b37e6ff62b35
Benjamin Bushman <benbushman98@gmail.com>
2022-07-25 10:35:29 -0600

    commented out API call

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,10 +1,1 @@
-const URL = "https://maps.googleapis.com/maps/api/place/nearbysearch/json?key=AIzaSyA8I6EN5t_ORE9DYQpOo6-LVpXfAeCp3SE&location=40.391617,-111.850769&radius=5000&type=restaurant";
-
-fetch(URL).then(data=> {
-  return data.json()
-}).then(jsonData => {
- console.log(jsonData.results)
-}).catch(error=> {
-  console.log(error);
-}) 
-
+// git a
