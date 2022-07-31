516a9d58785e3044be50980a4824ecc72cc6d2c1
Benjamin Bushman <benbushman98@gmail.com>
2022-07-31 13:28:47 -0600

    Fixed bugs in favorite locations

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,6 +4,11 @@ $('#cancel').click(function () {
     location.reload()
 })
 
+$('#clearlocalstorage').click(function() {
+    localStorage.clear();
+    location.reload();
+})
+
 // Pull from Local Storage and Render to Screen
 var lsSaveSearch = JSON.parse(window.localStorage.getItem("SavedCity")) || [];
 if (localStorage !== null) {
@@ -18,54 +23,64 @@ $('#savesearch').click(function () {
     var lsSaveSearch = JSON.parse(window.localStorage.getItem("SavedCity")) || [];
     var city = $("#city");
     var state = $("#state");
-    var stops = $("#number-of-stops");
+    var number = $("#number-of-stops");
     var favorites = {
         City: city.val(),
         State: state.val(),
-        Stops: stops.val()
+        Number: number.val()
     }
-    lsSaveSearch.push(favorites)
-    window.localStorage.setItem("SavedCity", JSON.stringify(lsSaveSearch))
-    favoriteSearch(lsSaveSearch)
+    if (favorites.City === "" || favorites.State === "" || favorites.Number === "") {
+        $('#alert').text("Please enter a City, State, and Number of Stops");
+        return;
+    } else {
+        lsSaveSearch.push(favorites);
+        window.localStorage.setItem("SavedCity", JSON.stringify(lsSaveSearch));
+        var city = favorites.City;
+        var state = favorites.State;
+        var number = favorites.Number;
+        favoriteSearch(lsSaveSearch);
+        cityUrlFunc(city, state, number);
+    }
 })
 
 function favoriteSearch(lsSaveSearch) {
     var savesearch = $('#savedcitybutton');
     savesearch.empty()
     for (var i = 0; i < lsSaveSearch.length; i++) {
-        if(lsSaveSearch[i] === "") {
-        } else{
-            savesearch.append('<button class="button favoritelocations" data-city = "' + lsSaveSearch[i].City + '" data-state = "' + lsSaveSearch[i].State + '" data-stops = "' + lsSaveSearch[i].Stops + '">' + lsSaveSearch[i].City + ", " + lsSaveSearch[i].State + " | " + lsSaveSearch[i].Stops + '</button>')
+        if (lsSaveSearch[i] === "") {
+        } else {
+            savesearch.append('<button class="button favoritelocations" data-city = "' + lsSaveSearch[i].City + '" data-state = "' + lsSaveSearch[i].State + '" data-number = "' + lsSaveSearch[i].Number + '">' + lsSaveSearch[i].City + ", " + lsSaveSearch[i].State + " | " + lsSaveSearch[i].Number + '</button>')
         }
     }
 }
 // End Function for Saved City
 
 // Favorite Locations API Calls
-$('.favoritelocations').click(function() {
+$('.favoritelocations').click(function () {
+    console.log("testing")
     var city = ($(this).attr("data-city"));
     var state = ($(this).attr("data-state"));
-    var number = ($(this).attr("data-stops"))
+    var number = ($(this).attr("data-number"))
 
     cityUrlFunc(city, state, number);
 })
 // End Favorite Locations API Calls
 
 // Button click after values are added
-$('#getit').click(function() {
+$('#submit').click(function () {
     var city = $('#city').val();
     var state = $('#state').val();
     var number = ($('#number-of-stops').val());
-    if (city === "" || state === "") {
-        $('#alert').text("Please enter a City or State")
+    if (city === "" || state === "" || number === "") {
+        $('#alert').text("Please enter a City, State, and Number of Stops")
     } else {
-        cityUrlFunc(city,state,number);
+        cityUrlFunc(city, state, number);
     }
 })
 // End Button click after values are added
 
 // Function for getting lat and lon
-function cityUrlFunc(city,state,number) {
+function cityUrlFunc(city, state, number) {
     var cityURL = 'https://api.openweathermap.org/geo/1.0/direct?q=' + city + ',' + state + ',usa&limit=1&appid=9c2f191921ea4a448012e7d41b8872c0';
 
     fetch(cityURL)
@@ -93,19 +108,19 @@ function runMapsApi(latitude, longitude, state, number) {
             return response.json();
         })
         .then(function (data) {
-            console.log(data.results);
+            // console.log(data.results);
             $('#cardcontainer').empty();
             // console.log(number)
 
             var randomNumber = [];
-            for (var a = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19], i = a.length; i--; ) {
+            for (var a = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19], i = a.length; i--;) {
                 var random = a.splice(Math.floor(Math.random() * (i + 1)), 1)[0];
                 console.log(random);
                 randomNumber[i] = random
             }
 
-                for (var j = 0; j < number; j++){
-               
+            for (var j = 0; j < number; j++) {
+
                 if (data.results[randomNumber[j]].business_status !== 'OPERATIONAL') {
                     number++
 
@@ -124,7 +139,7 @@ function runMapsApi(latitude, longitude, state, number) {
                     var cardStatus = (data.results[randomNumber[j]]?.opening_hours?.open_now);
                     if (cardPrice === undefined) {
                         cardPrice = ""
-                    } else if(cardPrice === 1) {
+                    } else if (cardPrice === 1) {
                         cardPrice = " - $"
                     } else if (cardPrice === 2) {
                         cardPrice = " - $$"
@@ -134,7 +149,7 @@ function runMapsApi(latitude, longitude, state, number) {
                         cardPrice = " - $$$$"
                     }
 
-                    if(cardRating <= 1.4) {
+                    if (cardRating <= 1.4) {
                         cardRating = "⭐"
                     } else if (cardRating <= 2.4) {
                         cardRating = "⭐⭐"
@@ -146,7 +161,7 @@ function runMapsApi(latitude, longitude, state, number) {
                         cardRating = "⭐⭐⭐⭐⭐"
                     }
 
-                    if(cardStatus === undefined) {
+                    if (cardStatus === undefined) {
                         cardStatus = "Hours Unknown"
                     } else if (cardStatus === true) {
                         cardStatus = "Open"
@@ -156,10 +171,11 @@ function runMapsApi(latitude, longitude, state, number) {
 
                     var card = $('<card />');
 
-                    $('#mainpage').hide()
-                    $('#startover').show()
+                    $('#mainpage').hide();
+                    $('#favlocationslist').hide();
+                    $('#startover').show();
                     $('#startover').click(function () {
-                        location.reload()
+                        location.reload();
                     })
                     card.attr("class", "card");
                     $('#cardcontainer').append(card);
@@ -169,11 +185,11 @@ function runMapsApi(latitude, longitude, state, number) {
                     $(card).append(photoEl);
                     $(card).append('<div id="cardAddress">' + cardAddress + '</div>');
                     $(card).append('<div id="cardStatus">' + "Status: " + cardStatus + '</div>');
-                    
+
                 }
-                }
-            })
-        }
+            }
+        })
+}
 
 // End Google Maps function for getting restaurants in the area
 
